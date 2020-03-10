3457bdde1f25b003af4686293441648a4a403b50
hisaichi5518 <hisaichi5518@gmail.com>
2020-03-10 22:59:32 +0900

    Update NativeWebViewPlugin

--- android/src/main/kotlin/jp/packages/native_webview/NativeWebviewPlugin.kt
+++ android/src/main/kotlin/jp/packages/native_webview/NativeWebviewPlugin.kt
@@ -1,52 +1,55 @@
 package jp.packages.native_webview
 
+import android.app.Activity
+import android.content.Context
 import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
+import io.flutter.plugin.platform.PlatformViewFactory
+import io.flutter.plugin.platform.PlatformViewRegistry
+import io.flutter.view.FlutterView
 
-/** NativeWebviewPlugin */
-public class NativeWebviewPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class NativeWebviewPlugin : FlutterPlugin, ActivityAware {
+    override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        Locator.binding = binding
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "packages.jp/native_webview")
-    channel.setMethodCallHandler(this);
-  }
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "packages.jp/native_webview")
-      channel.setMethodCallHandler(NativeWebviewPlugin())
+        onAttachedToEngine(
+            binding.binaryMessenger,
+            binding.platformViewRegistry
+        )
     }
-  }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        Locator.binding = null
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    private fun onAttachedToEngine(messenger: BinaryMessenger, registry: PlatformViewRegistry) {
+        registry.registerViewFactory(
+            "packages.jp/native_webview",
+            FlutterWebViewFactory(messenger)
+        )
+    }
+
+    override fun onDetachedFromActivity() {
+        Locator.activity = null
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        Locator.activity = binding.activity
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        Locator.activity = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        Locator.activity = null
+    }
 }
