8653d3f34e1731097366ffb2b3154ff895f47d3b
hisaichi5518 <hisaichi5518@gmail.com>
2020-09-19 17:51:25 +0900

    remove unnecessary code

--- lib/src/webview.dart
+++ lib/src/webview.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
